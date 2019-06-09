c6a8adbad681ff9fa72f847e8735c7d94afdd9bd
Edvaldo Silva <edvaldofullstack@gmail.com>
2019-06-08 22:18:35 -0300

    novo arquivo de projeto com configuração de seguraça

--- config/server.js
+++ config/server.js
@@ -3,6 +3,24 @@ const consign = require('consign');
 
 const app = express();
 
+// configuração de segurança
+
+app.disable('x-powered-by');
+
+app.get('*', (req, res, next) => {
+    if (req.headers['x-forwarded-proto'] != 'https') {
+        
+// checa se o header é HTTP ou HTTPS
+        res.redirect("https://" + req.headers.host + req.url);
+// faz o redirect para HTTPS
+
+    } else {
+        next();
+        
+// segue com a sequência das rotas
+    }
+});
+
 app.set('view engine', 'ejs');
 app.set('views' , './app/views');
 
