26cfbf1b53b8672a1384aedc2c1b28f71fc676bc
Edvaldo Silva <edvaldofullstack@gmail.com>
2019-06-11 05:44:51 -0300

    alteração pabrão

--- config/server.js
+++ config/server.js
@@ -6,17 +6,17 @@ const app = express();
 // configuração de segurança
 // OBS: Modo de segurança e certificado SSl
 
- app.disable('x-powered-by');
- app.get('*', (req, res, next) => {
-     if (req.headers['x-forwarded-proto'] != 'https') {  
+  app.disable('x-powered-by');
+  app.get('*', (req, res, next) => {
+      if (req.headers['x-forwarded-proto'] != 'https') {  
 // checa se o header é HTTP ou HTTPS
          res.redirect("https://" + req.headers.host + req.url);
 // faz o redirect para HTTPS
-     } else {
-         next();
+      } else {
+          next();
 // segue com a sequência das rotas
-    }
- });
+     }
+  });
 
 app.set('view engine', 'ejs');
 app.set('views' , './app/views');
